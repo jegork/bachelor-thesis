bfb9aceaa87fd79b32c22c47d63255473bea862d
Jegor Kitskerkin <jegor.kitskerkin@gmail.com>
2022-02-02 18:42:59 +0100

    Increase speed and performance
    - Use Tensorflow Dataset API
    - Add dropout
    - Change preprocessing

--- download.sh
+++
@@ -1,6 +0,0 @@
-apt-get update
-apt-get install unzip unrar
-wget --continue --progress=dot:mega --tries=0 https://www.crcv.ucf.edu/data/UCF101/UCF101.rar --no-check-certificate
-unrar x UCF101.rar
-wget https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip --no-check-certificate
-unzip UCF*.zip
